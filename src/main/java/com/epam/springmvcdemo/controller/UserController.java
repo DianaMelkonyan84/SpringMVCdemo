35429604dbfaa4c09a09febc925a7b35e1a60659
Diana Melkonay <dianamelkonyanit@gmail.com>
2023-11-11 20:12:29 +0400

    Initial commit

---
+++ src/main/java/com/epam/springmvcdemo/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.epam.springmvcdemo.controller;
+
+import com.epam.springmvcdemo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private Map<Integer, User> users = new HashMap<>();
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showUserPage(Model model){
+        model.addAttribute("user",new User());
+        model.addAttribute("users",users);
+        return "user-page";
+    }
+
+    @PostMapping
+    public String save(@ModelAttribute User user, Model model){
+        user.setId(users.size()+1);
+        users.put(user.getId(), user);
+
+        model.addAttribute("users",users);
+        model.addAttribute("user",new User());
+
+        System.out.println("Saved user: " + user.getId() + ", " + user.getUsername());
+
+        return "user-page";
+
+    }
+}
